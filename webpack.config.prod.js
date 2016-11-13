b168ced42c3f5ed8453e7a061ddd3905f3774fe6
Leonardo Faria <leonardofaria@gmail.com>
2016-11-12 23:55:35 -0800

    Update webpack and firebase settings

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,22 +1,22 @@
 const Dotenv = require('dotenv-webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const StatsPlugin = require('stats-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   // The entry file. All your app roots fromn here.
   entry: [
-    './app/App.js',
+    path.join(__dirname, './app/App.js'),
   ],
 
   // Where you want the output to go
   output: {
     path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
+    filename: '[name]-[hash].min.js',
     publicPath: '/',
   },
-
-  devtool: 'source-map',
 
   eslint: {
     configFile: './.eslintrc',
@@ -27,6 +27,11 @@ module.exports = {
       path: './.env', // if not simply .env
       safe: true, // lets load the .env.example file as well
     }),
+
+    // webpack gives your modules and chunks ids to identify them. Webpack can vary the
+    // distribution of the ids to get the smallest id length for often used ids with
+    // this plugin
+    new webpack.optimize.OccurenceOrderPlugin(),
 
     // handles creating an index.html file and injecting assets. necessary because assets
     // change name because the hash part changes. We want hash name changes to bust cache
@@ -41,19 +46,30 @@ module.exports = {
     // performance and is used in prod environments. Styles load faster on their own .css
     // file as they dont have to wait for the JS to load.
     new ExtractTextPlugin('[name]-[hash].min.css'),
+
+    // handles uglifying js
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true,
+      },
+    }),
+
+    // creates a stats.json
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false,
+    }),
+
+    // plugin for passing in data to the js, like what NODE_ENV we are in.
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
   ],
 
   module: {
-    preLoaders: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-    ],
-
+    // loaders handle the assets, like transforming sass to css or jsx to js.
     loaders: [
-      // loaders handle the assets, like transforming sass to css or jsx to js.
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -67,7 +83,7 @@ module.exports = {
       },
       {
         test: /\.json?$/,
-        loader: 'json'
+        loader: 'json',
       },
       {
         test: /\.(jpe?g|gif|png)$/,
@@ -82,6 +98,6 @@ module.exports = {
       normalize: path.join(__dirname, '/node_modules/normalize.css/normalize.css'),
       medium_editor: path.join(__dirname, '/node_modules/medium-editor/dist/css/medium-editor.css'),
       medium_editor_theme: path.join(__dirname, '/node_modules/medium-editor/dist/css/themes/default.css'),
-    }
+    },
   },
 };
