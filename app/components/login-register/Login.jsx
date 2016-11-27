7acd33f209efe1d6f44a509f57921a5c79c38da5
Leonardo Faria <leonardofaria@gmail.com>
2016-11-27 15:04:13 -0800

    Fix error messages in login/register forms

--- app/components/login-register/Login.jsx
+++ app/components/login-register/Login.jsx
@@ -5,82 +5,88 @@ const ReactRouter = require('react-router');
 
 const Link = ReactRouter.Link;
 
-const Login = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired,
-  },
-
-  getInitialState() {
-    return {
-      error: false,
-    };
-  },
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: false };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentWillMount() {
     document.body.classList.add('page-login');
-  },
+  }
 
   componentWillUnmount() {
     document.body.classList.remove('page-login');
-  },
+  }
 
   handleSubmit(e) {
     e.preventDefault();
-    const email = this.refs.email.value;
-    const pw = this.refs.pw.value;
-    const self = this;
+    const email = this.email.value;
+    const password = this.password.value;
 
-    const auth = firebase.auth().signInWithEmailAndPassword(email, pw).then(function(result) {
-      const location = self.props.location;
+    firebase.auth().signInWithEmailAndPassword(email, password).then((result) => {
+      const location = this.props.location;
+
       if (location.state && location.state.nextPathname) {
-        self.context.router.replace(location.state.nextPathname);
+        this.context.router.replace(location.state.nextPathname);
       } else {
-        self.context.router.replace('/dashboard');
+        this.context.router.replace('/dashboard');
       }
       // User signed in!
       // console.log('User signed in!');
       // var uid = result.user.uid;
-    }).catch(error => {
-      this.setState({ error: error });
+    }).catch((error) => {
+      this.setState({ error: error.message });
     });
-  },
+  }
 
   handleExternalLogin(provider) {
     const fb = new firebase.auth.FacebookAuthProvider();
-    const self = this;
 
-    const auth = firebase.auth().signInWithPopup(fb).then(function(result) {
-      const location = self.props.location;
+    firebase.auth().signInWithPopup(fb).then((result) => {
+      const location = this.props.location;
+
       if (location.state && location.state.nextPathname) {
-        self.context.router.replace(location.state.nextPathname);
+        this.context.router.replace(location.state.nextPathname);
       } else {
-        self.context.router.replace('/dashboard');
+        this.context.router.replace('/dashboard');
       }
       // User signed in!
       // console.log('User signed in!');
       // var uid = result.user.uid;
-    }).catch(function(error) {
-      this.setState({ error: error });
+    }).catch((error) => {
+      this.setState({ error: error.message });
     });
-  },
+  }
 
   render() {
     const errors = this.state.error ? <p> {this.state.error} </p> : '';
+
     return (
       <div className="text-center">
         <form className="form form-account" onSubmit={this.handleSubmit}>
-          <input className="form-control" ref="email" placeholder="Email" />
-          <input ref="pw" type="password" className="form-control" placeholder="Password" />
           {errors}
-          <button type="submit" className="btn btn-primary">Login</button><br />
 
+          <input ref={(c) => { this.email = c; }} className="form-control" placeholder="Email" />
+          <input ref={(c) => { this.password = c; }} type="password" className="form-control" placeholder="Password" />
+
+          <button type="submit" className="btn btn-primary">Login</button><br />
           <button type="button" className="btn btn-facebook" onClick={() => this.handleExternalLogin('facebook')}>Login Facebook</button>
 
           <Link to="/register">Register</Link>
         </form>
       </div>
     );
-  },
-});
+  }
+}
+
+Login.propTypes = {
+  location: React.PropTypes.object.isRequired,
+};
+
+Login.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+};
 
 module.exports = Login;
--- app/components/login-register/Register.jsx
+++ app/components/login-register/Register.jsx
@@ -5,49 +5,51 @@ const ReactRouter = require('react-router');
 
 const Link = ReactRouter.Link;
 
-const Register = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired,
-  },
-
-  getInitialState() {
-    return {
-      error: false,
-    };
-  },
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { error: false };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
   componentWillMount() {
     document.body.classList.add('page-register');
-  },
+  }
 
   componentWillUnmount() {
     document.body.classList.remove('page-register');
-  },
+  }
 
   handleSubmit(e) {
     e.preventDefault();
-    const email = this.refs.email.value;
-    const pw = this.refs.pw.value;
+    const email = this.email.value;
+    const password = this.password.value;
 
-    firebase.auth().createUserWithEmailAndPassword(email, pw)
-    .then(this.context.router.replace('/'))
-    .catch(this.setState({ error: e.message }));
-  },
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(this.context.router.replace('/dashboard'))
+      .catch(this.setState({ error: e.message }));
+  }
 
   render() {
     const errors = this.state.error ? <p> {this.state.error} </p> : '';
     return (
       <div className="text-center">
         <form className="form form-account" onSubmit={this.handleSubmit}>
-          <input className="form-control" ref="email" placeholder="Email"/>
-          <input ref="pw" type="password" className="form-control" placeholder="Password" />
           {errors}
+
+          <input ref={(c) => { this.email = c; }} className="form-control" placeholder="Email" />
+          <input ref={(c) => { this.password = c; }} type="password" className="form-control" placeholder="Password" />
+
           <button type="submit" className="btn btn-primary">Register</button>
           <Link to="/login">Login</Link>
         </form>
       </div>
     );
-  },
-});
+  }
+}
+
+Register.contextTypes = {
+  router: React.PropTypes.object.isRequired,
+};
 
 module.exports = Register;
